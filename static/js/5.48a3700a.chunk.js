1ce3fd70cfaef16c6fd37961178a126584016a5c
Shaked Ashkenazi <ash.shaked@gmail.com>
2021-02-05 00:34:46 +0200

    Updates

--- static/js/5.48a3700a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpplanatrip=this.webpackJsonpplanatrip||[]).push([[5],{48:function(e,n,s){"use strict";s.r(n),s.d(n,"default",(function(){return a}));var t=s(7);s(1);function a(){return Object(t.jsx)("div",{className:"App",children:Object(t.jsx)("div",{className:"App-header",children:Object(t.jsx)("p",{children:"Thanks!"})})})}}}]);
-//# sourceMappingURL=5.48a3700a.chunk.js.map
